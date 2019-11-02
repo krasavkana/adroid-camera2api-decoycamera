8a6ef4f08c0cafe78e18c8494c49bff348f039cf
KrasavKana <krasavkana@gmail.com>
2019-11-02 16:20:11 +0900

    メイン画面右上にあるアクションバーからメニュー経由で設定画面を操作できるようにした。音声マスタアプリを呼び出す設定を含み数個の設定を可能にした。

--- Application/src/main/java/com/krasavkana/android/decoycamera/Camera2BasicFragment.java
+++ Application/src/main/java/com/krasavkana/android/decoycamera/Camera2BasicFragment.java
@@ -22,6 +22,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -58,6 +59,7 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 //import android.support.v4.app.Fragment;
 import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 //import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.util.Size;
@@ -109,6 +111,9 @@ public class Camera2BasicFragment extends Fragment
      * Tag for the {@link Log}.
      */
     private static final String TAG = "Camera2BasicFragment";
+
+
+    SharedPreferences mPref;
 
     /**
      * Camera state: Showing camera preview.
@@ -250,6 +255,7 @@ public class Camera2BasicFragment extends Fragment
      * This is the output file for our picture.
      */
     private File mFile;
+    private String mPrefix;
 
     /**
      * This a callback object for the {@link ImageReader}. "onImageAvailable" will be called when a
@@ -260,7 +266,7 @@ public class Camera2BasicFragment extends Fragment
 
         @Override
         public void onImageAvailable(ImageReader reader) {
-            mFile = new File(getActivity().getExternalFilesDir(null), "pic" + getNowTimestamp() + ".jpg");
+            mFile = new File(getActivity().getExternalFilesDir(null), mPrefix + getNowTimestamp() + ".jpg");
             mBackgroundHandler.post(new ImageSaver(reader.acquireNextImage(), mFile));
         }
 
@@ -475,7 +481,7 @@ public class Camera2BasicFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.e(TAG, "onCreateView()");
+        Log.d(TAG, "onCreateView()");
         final View v = inflater.inflate(R.layout.fragment_camera2_basic, container, false);
 
         // View#setFocusableInTouchModeでtrueをセットしておくこと
@@ -486,7 +492,7 @@ public class Camera2BasicFragment extends Fragment
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 // KeyEvent.ACTION_DOWN以外のイベントを無視する
                 // （これがないとKeyEvent.ACTION_UPもフックしてしまう）
-                Log.e(TAG, "onKey()");
+                Log.d(TAG, "onKey()");
                 if(event.getAction() != KeyEvent.ACTION_DOWN) {
                     return false;
                 }
@@ -509,6 +515,7 @@ public class Camera2BasicFragment extends Fragment
 
     @Override
     public void onViewCreated(final View view, Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated()");
         view.findViewById(R.id.picture).setOnClickListener(this);
         view.findViewById(R.id.info).setOnClickListener(this);
         mTextureView = (AutoFitTextureView) view.findViewById(R.id.texture);
@@ -522,16 +529,23 @@ public class Camera2BasicFragment extends Fragment
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated()");
         super.onActivityCreated(savedInstanceState);
-
-        mLensFacingFront = false;// initially Lens Facing is back
+        mPref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String aaa = mPref.getString("preference_theme", "");
+        Log.d(TAG, "preference_theme:" + aaa);
+        mLensFacingFront = mPref.getBoolean("preference_front_lens_facing", false);
+        Log.d(TAG, "mLensFacingFront:" + mLensFacingFront);
+        mPrefix = mPref.getString("preference_save_prefix", "");
+        Log.d(TAG, "mPrefix:" + mPrefix);
+//        mLensFacingFront = false;// initially Lens Facing is back
 //        mLensFacingFront = true;// initially Lens Facing is back
-
 //        mFile = new File(getActivity().getExternalFilesDir(null), "pic" + getNowTimestamp() + ".jpg");
     }
 
     @Override
     public void onResume() {
+        Log.d(TAG, "onResume()");
         super.onResume();
         mTextureView.setVisibility(View.VISIBLE);
         startBackgroundThread();
@@ -552,6 +566,7 @@ public class Camera2BasicFragment extends Fragment
     //https://stackoverflow.com/questions/42510285/java-lang-illegalstateexception-session-has-been-closed-further-changes-are-il?rq=1
     @Override
     public void onPause() {
+        Log.d(TAG, "onPause()");
         super.onPause();
         mTextureView.setVisibility(View.GONE);
         closeCamera();
@@ -595,6 +610,7 @@ public class Camera2BasicFragment extends Fragment
      */
     @SuppressWarnings("SuspiciousNameCombination")
     private void setUpCameraOutputs(int width, int height) {
+        Log.d(TAG, "setUpCameraOutputs()");
         Activity activity = getActivity();
         CameraManager manager = (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
         try {
@@ -710,6 +726,7 @@ public class Camera2BasicFragment extends Fragment
      * Opens the camera specified by {@link Camera2BasicFragment#mCameraId}.
      */
     private void openCamera(int width, int height) {
+        Log.d(TAG, "openCamera()");
         // comment out to avoid to get this message so often
         // E/CheckPermission: real_camera-code= 12
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
@@ -737,6 +754,7 @@ public class Camera2BasicFragment extends Fragment
      * Closes the current {@link CameraDevice}.
      */
     private void closeCamera() {
+        Log.d(TAG, "closeCamera()");
         try {
             mCameraOpenCloseLock.acquire();
             if (null != mCaptureSession) {
@@ -785,6 +803,7 @@ public class Camera2BasicFragment extends Fragment
      * Creates a new {@link CameraCaptureSession} for camera preview.
      */
     private void createCameraPreviewSession() {
+        Log.d(TAG, "createCameraPreviewSession()");
         try {
             SurfaceTexture texture = mTextureView.getSurfaceTexture();
             assert texture != null;
@@ -851,6 +870,7 @@ public class Camera2BasicFragment extends Fragment
      * @param viewHeight The height of `mTextureView`
      */
     private void configureTransform(int viewWidth, int viewHeight) {
+        Log.d(TAG, "configureTransform()");
         Activity activity = getActivity();
         if (null == mTextureView || null == mPreviewSize || null == activity) {
             return;
@@ -886,6 +906,7 @@ public class Camera2BasicFragment extends Fragment
      * Lock the focus as the first step for a still image capture.
      */
     private void lockFocus() {
+        Log.d(TAG, "lockFocus()");
         try {
             // This is how to tell the camera to lock focus.
             mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
@@ -905,7 +926,7 @@ public class Camera2BasicFragment extends Fragment
      * we get a response in {@link #mCaptureCallback} from {@link #lockFocus()}.
      */
     private void runPrecaptureSequence() {
-        Log.d(TAG, "runPrecaptureSequence");
+        Log.d(TAG, "runPrecaptureSequence()");
         try {
             // This is how to tell the camera to trigger.
             mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER,
@@ -924,7 +945,7 @@ public class Camera2BasicFragment extends Fragment
      * {@link #mCaptureCallback} from both {@link #lockFocus()}.
      */
     private void captureStillPicture() {
-        Log.d(TAG, "captureStillPicture");
+        Log.d(TAG, "captureStillPicture()");
         try {
             final Activity activity = getActivity();
             if (null == activity || null == mCameraDevice) {
@@ -985,6 +1006,7 @@ public class Camera2BasicFragment extends Fragment
      * finished.
      */
     private void unlockFocus() {
+        Log.d(TAG, "unlockFocus()");
         try {
             // Reset the auto-focus trigger
             mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
--- Application/src/main/java/com/krasavkana/android/decoycamera/CameraActivity.java
+++ Application/src/main/java/com/krasavkana/android/decoycamera/CameraActivity.java
@@ -1,17 +1,28 @@
 package com.krasavkana.android.decoycamera;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -20,22 +31,37 @@ import java.io.InputStream;
 
 public class CameraActivity extends AppCompatActivity {
 
-//    private Bitmap image;
-//    private boolean isFinished = false;
-//    private File[] mFiles;
-//    private int mFileNum = 0;
-//    private int mFilePos = -1;
-//    private ImageView mImageView;
+    private Bitmap image;
+    private boolean isFinished = false;
+    private File[] mFiles;
+    private int mFileNum = 0;
+    private int mFilePos = -1;
+    private ImageView mImageView;
 
     /**
      * Tag for the {@link Log}.
      */
     private static final String TAG = "CameraActivity";
 
+    private static final String CAMOUFLAGE_MODE = "imageview";
+
+    private final static String MASTER_MUTE_PACKAGE_NAME = "com.krasavkana.android.mastermute";
+    private final static String MASTER_MUTE_CLASS_NAME = "com.krasavkana.android.mastermute.MainActivity";
+    private static final int REQUEST_CODE_MASTER_MUTE = 1;
+
+    SharedPreferences mPref;
+    String mCamouflage;
+    long mImageViewInterval;
+    boolean mMasterMuteOn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(R.string.shortAppName);
         if (null == savedInstanceState) {
             Intent intent = getIntent();
             String bleCommand;
@@ -49,25 +75,51 @@ public class CameraActivity extends AppCompatActivity {
                     .commit();
         }
 
-        // get file list from the external storage path
-//        String path = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath();
-//        if (path == null ){
-//            Toast.makeText(this, "Check external file dir.", Toast.LENGTH_SHORT).show();
-//            finish();
-//        }
-//        mFiles = new File(path).listFiles();
-//        mFileNum = mFiles.length;
-//        if (mFileNum == 0 ) {
-//            Toast.makeText(this, "Camouflage images not found.", Toast.LENGTH_SHORT).show();
-//            finish();
-//        }
-//        Log.d(TAG, "Num of image files: " + mFileNum);
-//        if (savedInstanceState != null) {
-//            mFilePos = -1;
-//        }else{
-//            mFilePos = -1;
-//        }
-//        mImageView = findViewById(R.id.image_view);
+        mPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mImageViewInterval = Long.parseLong(mPref.getString("preference_imageview_interval", "10000"));
+        Log.d(TAG, "mImageViewInterval:" + mImageViewInterval);
+
+        mCamouflage = mPref.getString("preference_camouflage", "");
+        Log.d(TAG, "mCamouflage:" + mCamouflage);
+        if(CAMOUFLAGE_MODE.equals(mCamouflage)) {
+
+            // get file list from the external storage path
+
+            String path = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath();
+            if (path == null) {
+                Toast.makeText(this, "Check external file dir.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            mFiles = new File(path).listFiles();
+            mFileNum = mFiles.length;
+            if (mFileNum == 0) {
+                Toast.makeText(this, "Camouflage images not found.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            Log.d(TAG, "Num of image files: " + mFileNum);
+            if (savedInstanceState != null) {
+                mFilePos = -1;
+            } else {
+                mFilePos = -1;
+            }
+            mImageView = findViewById(R.id.image_view);
+        }
+
+        mMasterMuteOn = mPref.getBoolean("preference_master_mute_on", false);
+        Log.d(TAG, "mMasterMuteOn:" + (mMasterMuteOn?"true":"false"));
+        if(mMasterMuteOn) {
+            Intent intent = new Intent();
+            intent.setClassName(MASTER_MUTE_PACKAGE_NAME, MASTER_MUTE_CLASS_NAME);
+
+            try {
+                startActivityForResult(intent, REQUEST_CODE_MASTER_MUTE);
+            } catch (ActivityNotFoundException e) {
+                Log.d(TAG, "Intent to be invoked NOT found");
+                Toast.makeText(this, "Intent to be invoked NOT found", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
     @Override
     public void onPause() {
@@ -76,10 +128,26 @@ public class CameraActivity extends AppCompatActivity {
 //        isFinished = true;
     }
     @Override
+    public void onStart() {
+        Log.d(TAG, "onStart()");
+        super.onStart();
+        FrameLayout root = findViewById(R.id.container);
+//        mPref = PreferenceManager.getDefaultSharedPreferences(this);
+//        switch (mPref.getString("preference_theme", getString(R.string.default_value_preference_theme))) {
+//            case "light":
+//                root.setBackgroundColor(Color.parseColor("#FFFFFF"));
+//                break;
+//            case "dark":
+//                root.setBackgroundColor(Color.parseColor("#000000"));
+//                break;
+//        }
+        isFinished = true;
+    }
+    @Override
     public void onStop() {
         Log.d(TAG, "onStop()");
         super.onStop();
-//        isFinished = true;
+        isFinished = true;
     }
     @Override
     public void onResume() {
@@ -87,39 +155,87 @@ public class CameraActivity extends AppCompatActivity {
         super.onResume();
 
         // set handler to get image for imageview
-//        final Handler handler = new Handler();
-//        handler.post( new Runnable() {
-//            @Override
-//            public void run() {
-//                mFilePos++;
-//                if (mFilePos >= mFileNum){
-//                    mFilePos = 0;
-//                }
-//
-//                Log.d(TAG, "Current Pos in image list: " + mFilePos);
-////                Log.d(TAG, mFiles[mFilePos].getPath());
-//
-//                try (InputStream inputStream0 =
-//                             new FileInputStream(mFiles[mFilePos])) {
-//                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream0);
-//                    mImageView.setImageBitmap(bitmap);
-//                    mImageView.invalidate();
-//                    handler.postDelayed(this, 20000);
-//                    if (isFinished) {
-//                        handler.removeCallbacks(this);
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//
-//        isFinished = false;
+        final Handler handler = new Handler();
+        if(mImageView != null) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mFilePos++;
+                    if (mFilePos >= mFileNum) {
+                        mFilePos = 0;
+                    }
+
+                    Log.d(TAG, "Current Pos in image list: " + mFilePos);
+//                Log.d(TAG, mFiles[mFilePos].getPath());
+
+                    try (InputStream inputStream0 =
+                                 new FileInputStream(mFiles[mFilePos])) {
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream0);
+                        mImageView.setImageBitmap(bitmap);
+                        mImageView.invalidate();
+                        handler.postDelayed(this, mImageViewInterval);
+                        if (isFinished) {
+                            handler.removeCallbacks(this);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            isFinished = false;
+        }
     }
     @Override
     public void onDestroy() {
         Log.d(TAG, "onDestroy()");
         super.onDestroy();
-//        isFinished = true;
+        isFinished = true;
     }
+
+    // メニューをActivity上に設置する
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 参照するリソースは上でリソースファイルに付けた名前と同じもの
+        getMenuInflater().inflate(R.menu.option, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // メニューが選択されたときの処理
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItem1:
+                startActivity(new Intent(CameraActivity.this, SettingsActivity.class));
+                return true;
+
+            case R.id.menuItem2:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // startActivityForResult起動後に値が返されたときの処理
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            //SecondActivityから戻ってきた場合
+            case (REQUEST_CODE_MASTER_MUTE):
+                if (resultCode == RESULT_OK) {
+                    Log.d(TAG, "onActivityResult():RESULT_OK");
+                } else if (resultCode == RESULT_CANCELED) {
+                    Log.d(TAG, "onActivityResult():RESULT_CANCELED");
+                    Toast.makeText(this, "MasterMute has to be ON, or Die", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    //その他
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
 }
